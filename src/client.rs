52746c2f3a7078ae9570c2eeab8e8aaf3f918bd4
juanpalopez <juanpablolopez@outlook.com>
2023-12-12 22:42:21 +0000

    [feat][support-vs-currencies] Adding logic I adding support for the support-vs-currencies which map to /simple/supported_vs_currencies endpoint from CoinGecko API

--- src/client.rs
+++ src/client.rs
@@ -9,23 +9,46 @@ pub enum CryptoClientStatus {
     Unknown,
 }
 
-#[derive(Debug)]
 pub struct CryptoClientRes {
     status: CryptoClientStatus,
+    body: String,
 }
 
 impl CryptoClientRes {
     pub fn get_status(&self) -> CryptoClientStatus {
         self.status
     }
+
+    pub fn get_body(&self) -> &String {
+        &self.body
+    }
 }
 
 #[async_trait]
 pub trait CryptoClient {
     async fn ping(&self) -> Result<CryptoClientRes>;
+    async fn simple_supported_vs_currencies(&self) -> Result<CryptoClientRes>;
 }
 
 pub struct CryptoClientHTTP;
+
+impl CryptoClientHTTP {
+    async fn get(&self, endpoint: &str) -> Result<CryptoClientRes> {
+        let client = Client::new();
+        let url = format!("{}/{}", Self::BASE_API_URL, endpoint);
+        let response = client.get(url).send().await?;
+        let status = response.status();
+        let body = response.text().await?;
+
+        let status = match &status.as_u16() {
+            200..=299 => CryptoClientStatus::Success,
+            300..=599 => CryptoClientStatus::Error,
+            _ => CryptoClientStatus::Unknown,
+        };
+
+        Ok(CryptoClientRes { status, body })
+    }
+}
 
 impl CryptoClientHTTP {
     const BASE_API_URL: &'static str = "https://api.coingecko.com/api/v3";
@@ -34,16 +57,12 @@ impl CryptoClientHTTP {
 #[async_trait]
 impl CryptoClient for CryptoClientHTTP {
     async fn ping(&self) -> Result<CryptoClientRes> {
-        let client = Client::new();
-        let url = format!("{}/ping", Self::BASE_API_URL);
-        let response = client.get(url).send().await?;
+        let response = self.get("/ping").await?;
+        Ok(response)
+    }
 
-        let status = match &response.status().as_u16() {
-            200..=299 => CryptoClientStatus::Success,
-            300..=599 => CryptoClientStatus::Error,
-            _ => CryptoClientStatus::Unknown,
-        };
-
-        Ok(CryptoClientRes { status })
+    async fn simple_supported_vs_currencies(&self) -> Result<CryptoClientRes> {
+        let response = self.get("/simple/supported_vs_currencies").await?;
+        Ok(response)
     }
 }
--- src/simple.rs
+++ src/simple.rs
@@ -1,64 +1,90 @@
 use clap::{Parser, Subcommand};
+use serde_json::Value;
+
+#[path = "./client.rs"]
+mod client;
+use client::CryptoClient;
 
 #[derive(Debug, Parser)]
-pub struct SimpleCtx{
-	#[command(subcommand)]
-	commands: Commands,
+pub struct SimpleCtx {
+    #[command(subcommand)]
+    commands: Commands,
 }
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-	/// Get the current price of any cryptocurrencies in any other supported currencies that you need.
-	Price(PriceCtx),
-	/// Get current price of tokens (using contract addresses) for a 
-	/// given platform in any other currency that you need.
-	TokenPrice(TokenPriceCtx),
-	/// Get list of supported_vs_currencies.
-	SupportedVsCurrencies(SupportedVsCurrenciesCtx),
+    /// Get the current price of any cryptocurrencies in any other supported currencies that you need.
+    Price(PriceCtx),
+    /// Get current price of tokens (using contract addresses) for a
+    /// given platform in any other currency that you need.
+    TokenPrice(TokenPriceCtx),
+    /// Get list of supported_vs_currencies.
+    SupportedVsCurrencies(SupportedVsCurrenciesCtx),
 }
 
 #[derive(Debug, Parser)]
 pub struct SupportedVsCurrenciesCtx {}
 
 impl SupportedVsCurrenciesCtx {
-	pub async fn run_command(&self) {
-		println!("SupporedVsCurrencies...");
-	}
+    fn parse_response(&self, body: &String) -> Vec<String> {
+		let data = serde_json::from_str(body.as_str()).unwrap();
+        let mut result = Vec::new();
+
+		if let Value::Array(arr) = data {
+			for currency in arr {
+				if let Some(c) = currency.as_str() {
+					result.push(c.to_string()); 
+				}
+			}
+		}
+
+        result
+    }
+    pub async fn run_command(&self) {
+        let crypto_client = client::CryptoClientHTTP;
+
+        let client_res = match crypto_client.simple_supported_vs_currencies().await {
+            Ok(res) => res,
+            Err(error) => panic!("Problem with ping {}", error),
+        };
+        let currencies = self.parse_response(client_res.get_body());
+
+        for currency in currencies {
+            println!("{}", currency);
+        }
+    }
 }
 
 #[derive(Debug, Parser)]
-pub struct PriceCtx{}
+pub struct PriceCtx {}
 
 impl PriceCtx {
-	pub async fn run_command(&self) {
-		println!("Price...")
-	}
+    pub async fn run_command(&self) {
+        println!("Price...")
+    }
 }
 
 #[derive(Debug, Parser)]
-pub struct  TokenPriceCtx{}
+pub struct TokenPriceCtx {}
 
 impl TokenPriceCtx {
-	pub async fn run_command(&self) {
-		println!("TokenPrice...")
-	}
+    pub async fn run_command(&self) {
+        println!("TokenPrice...")
+    }
 }
 
-
-
 impl SimpleCtx {
-	pub async fn run_command(&self) {
-
-		match &self.commands {
-			Commands::SupportedVsCurrencies(ctx) => {
-				SupportedVsCurrenciesCtx::run_command(ctx).await;
-			},
-			Commands::Price(ctx) => {
-				PriceCtx::run_command(ctx).await;
-			},
-			Commands::TokenPrice(ctx) => {
-				TokenPriceCtx::run_command(ctx).await;
-			},
-		};
-	}
+    pub async fn run_command(&self) {
+        match &self.commands {
+            Commands::SupportedVsCurrencies(ctx) => {
+                SupportedVsCurrenciesCtx::run_command(ctx).await;
+            }
+            Commands::Price(ctx) => {
+                PriceCtx::run_command(ctx).await;
+            }
+            Commands::TokenPrice(ctx) => {
+                TokenPriceCtx::run_command(ctx).await;
+            }
+        };
+    }
 }
